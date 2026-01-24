e3f84d42ee0d481ea492ff4432d0655a03a542f5
jeel-gajera-prominentpixel <jeel.gajera@prominentpixel.com>
2026-01-24 15:40:44 +0530

    eb_name removed from local

--- terraform-aws-eventbridge-wrapper/locals.tf
+++ terraform-aws-eventbridge-wrapper/locals.tf
@@ -1,7 +1,4 @@
 locals {
-  # Generates the EventBridge name using company prefix, application name, region suffix, and environment.
-  eb_name = var.name
-
   # Default tags that will be applied to all resources unless overridden by custom tags.
   tags = {
     company     = var.company
