662caf1550e706a97f77b9cf485f3d321bb11a48
jeel-gajera-prominentpixel <jeel.gajera@prominentpixel.com>
2026-01-24 14:47:31 +0530

    unnecessary variables removed

--- terraform-aws-eventbridge-wrapper/locals.tf
+++ terraform-aws-eventbridge-wrapper/locals.tf
@@ -3,5 +3,11 @@ locals {
   eb_name = var.name
 
   # Default tags that will be applied to all resources unless overridden by custom tags.
-  tags = module.tags.tags
+  tags = {
+    company     = var.company
+    environment = var.environment
+    project     = var.project
+    repository  = var.repository_url
+    region      = var.region 
+  }
 }
--- terraform-aws-eventbridge-wrapper/variables.tf
+++ terraform-aws-eventbridge-wrapper/variables.tf
@@ -10,26 +10,6 @@ variable "project" {
   default     = ""
 }
 
-variable "company_prefix" {
-  description = "A prefix representing the company, used for resource naming conventions."
-  type        = string
-}
-
-variable "company" {
-  description = "Name the company, used for resource naming conventions."
-  type        = string
-}
-
-variable "environment" {
-  description = "The deployment environment of the resource (e.g., dev, staging, prod)."
-  type        = string
-}
-
-variable "region_suffix" {
-  description = "A suffix representing the AWS region (e.g., 'use1' for US East 1, 'usw1' for US West 1)."
-  type        = string
-}
-
 variable "region" {
   description = "Region of the resource."
   type        = string
