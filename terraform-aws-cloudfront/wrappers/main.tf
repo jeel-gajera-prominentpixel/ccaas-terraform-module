5c3a20c50dead3fc02db426011bf4a66a8bfae16
jeel-gajera-prominentpixel <jeel.gajera@prominentpixel.com>
2026-01-28 14:51:05 +0530

    wrapper customized according to needs

--- terraform-aws-cloudfront/wrappers/main.tf
+++ terraform-aws-cloudfront/wrappers/main.tf
@@ -1,45 +1,36 @@
-module "wrapper" {
+module "cloudfront" {
   source = "../"
-
-  for_each = var.items
-
-  aliases                         = try(each.value.aliases, var.defaults.aliases, null)
-  anycast_ip_list_id              = try(each.value.anycast_ip_list_id, var.defaults.anycast_ip_list_id, null)
-  cloudfront_functions            = try(each.value.cloudfront_functions, var.defaults.cloudfront_functions, null)
-  comment                         = try(each.value.comment, var.defaults.comment, null)
-  continuous_deployment_policy_id = try(each.value.continuous_deployment_policy_id, var.defaults.continuous_deployment_policy_id, null)
-  create                          = try(each.value.create, var.defaults.create, true)
-  create_monitoring_subscription  = try(each.value.create_monitoring_subscription, var.defaults.create_monitoring_subscription, false)
-  custom_error_response           = try(each.value.custom_error_response, var.defaults.custom_error_response, null)
-  default_cache_behavior          = try(each.value.default_cache_behavior, var.defaults.default_cache_behavior)
-  default_root_object             = try(each.value.default_root_object, var.defaults.default_root_object, null)
-  enabled                         = try(each.value.enabled, var.defaults.enabled, true)
-  http_version                    = try(each.value.http_version, var.defaults.http_version, "http2")
-  is_ipv6_enabled                 = try(each.value.is_ipv6_enabled, var.defaults.is_ipv6_enabled, true)
-  logging_config                  = try(each.value.logging_config, var.defaults.logging_config, null)
-  ordered_cache_behavior          = try(each.value.ordered_cache_behavior, var.defaults.ordered_cache_behavior, [])
-  origin                          = try(each.value.origin, var.defaults.origin, {})
-  origin_access_control = try(each.value.origin_access_control, var.defaults.origin_access_control, {
-    s3 = {
-      origin_type      = "s3",
-      signing_behavior = "always",
-      signing_protocol = "sigv4"
-    }
-  })
-  origin_group                         = try(each.value.origin_group, var.defaults.origin_group, null)
-  price_class                          = try(each.value.price_class, var.defaults.price_class, null)
-  realtime_metrics_subscription_status = try(each.value.realtime_metrics_subscription_status, var.defaults.realtime_metrics_subscription_status, "Enabled")
-  response_headers_policies            = try(each.value.response_headers_policies, var.defaults.response_headers_policies, null)
+  aliases                              = var.aliases
+  anycast_ip_list_id                   = var.anycast_ip_list_id
+  cloudfront_functions                 = var.cloudfront_functions
+  comment                              = var.comment
+  continuous_deployment_policy_id      = var.continuous_deployment_policy_id
+  create                               = var.create
+  create_monitoring_subscription       = var.create_monitoring_subscription
+  custom_error_response                = var.custom_error_response
+  default_cache_behavior               = var.default_cache_behavior
+  default_root_object                  = var.default_root_object
+  enabled                              = var.enabled
+  http_version                         = var.http_version
+  is_ipv6_enabled                      = var.is_ipv6_enabled
+  logging_config                       = var.logging_config
+  ordered_cache_behavior               = var.ordered_cache_behavior
+  origin                               = var.origin
+  origin_access_control                = var.origin_access_control
+  origin_group                         = var.origin_group
+  price_class                          = var.price_class
+  realtime_metrics_subscription_status = var.realtime_metrics_subscription_status
+  response_headers_policies            = var.response_headers_policies
   restrictions = try(each.value.restrictions, var.defaults.restrictions, {
     geo_restriction = {
       restriction_type = "none"
     }
   })
-  retain_on_delete    = try(each.value.retain_on_delete, var.defaults.retain_on_delete, null)
-  staging             = try(each.value.staging, var.defaults.staging, null)
-  tags                = try(each.value.tags, var.defaults.tags, {})
-  viewer_certificate  = try(each.value.viewer_certificate, var.defaults.viewer_certificate, {})
-  vpc_origin          = try(each.value.vpc_origin, var.defaults.vpc_origin, null)
-  wait_for_deployment = try(each.value.wait_for_deployment, var.defaults.wait_for_deployment, null)
-  web_acl_id          = try(each.value.web_acl_id, var.defaults.web_acl_id, null)
+  retain_on_delete    = var.retain_on_delete
+  staging             = var.staging
+  viewer_certificate  = var.viewer_certificate
+  vpc_origin          = var.vpc_origin
+  wait_for_deployment = var.wait_for_deployment
+  web_acl_id          = var.web_acl_id
+  tags                = merge(local.tags, var.tags)
 }
--- terraform-aws-cloudfront/wrappers/outputs.tf
+++ terraform-aws-cloudfront/wrappers/outputs.tf
@@ -1,9 +1,3 @@
-output "wrapper" {
-  description = "Map of outputs of a wrapper."
-  value       = module.wrapper
-  # sensitive = false # No sensitive module output found
-}
-
 output "cloudfront_distribution_id" {
   description = "The identifier for the distribution."
   value       = module.wrapper.cloudfront_distribution_id
