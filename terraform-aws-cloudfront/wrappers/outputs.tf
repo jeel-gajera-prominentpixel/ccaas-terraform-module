a4402638a8fe0e709f4b19682e1f3c1bb26794c3
jeel-gajera-prominentpixel <jeel.gajera@prominentpixel.com>
2026-01-28 12:26:49 +0530

    wrapper output configured

--- terraform-aws-cloudfront/wrappers/outputs.tf
+++ terraform-aws-cloudfront/wrappers/outputs.tf
@@ -3,3 +3,13 @@ output "wrapper" {
   value       = module.wrapper
   # sensitive = false # No sensitive module output found
 }
+
+output "cloudfront_distribution_id" {
+  description = "The identifier for the distribution."
+  value       = module.wrapper.cloudfront_distribution_id
+}
+
+output "cloudfront_distribution_arn" {
+  description = "The ARN (Amazon Resource Name) for the distribution."
+  value       = module.wrapper.cloudfront_distribution_arn
+}
